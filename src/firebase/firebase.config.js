8935bffd5847d1710a7b1335fc5e269bcb9ff838
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-08 16:44:15 +0600

    remove firebase comment

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -14,7 +14,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
