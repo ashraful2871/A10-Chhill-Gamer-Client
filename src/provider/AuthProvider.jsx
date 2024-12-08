5550918e30e3c30ae068eb0085d71b711e6373ae
ashraful2871 <ashrafulislam2758@gmail.com>
2024-12-08 17:08:17 +0600

    remove authprovider file all comments

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -18,36 +18,30 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  //sign up
   const signUp = (email, password) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  //login
   const login = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
-  //google login
   const googleLogin = () => {
     setLoading(true);
     return signInWithPopup(auth, provider);
   };
 
-  //sign Out
   const signOutUser = () => {
     setLoading(true);
     return signOut(auth);
   };
 
-  // update profile
   const updateUserProfile = (updatedDate) => {
     return updateProfile(auth.currentUser, updatedDate);
   };
 
-  //observer
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
